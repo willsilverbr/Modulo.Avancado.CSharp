20ee716743f8dbbde916798355575b69ac2495ef
William Barbosa <will.silver@hotmail.com>
2022-02-20 21:22:24 -0300

    PRojetos de estudo Modulo Avançado: Linq.Group Linq.Join Linq.Lambda.Selec Linq.Objeto Linq.Sintaxe.Alternativa

--- Linq.Group/Program.cs
+++ Linq.Group/Program.cs
@@ -8,7 +8,7 @@ namespace Linq.Group
         static void Main(string[] args)
         {
             int[] listaNum = { 1, 1, 1, 4, 4, 4, 2, 3, 5, 6, 6, 10, 9, 8, };
-
+            //
             //Group - Distinct
             //var listaFiltrada = listaNum.Distinct().OrderBy(a => a).Select(a => a);
             //var listaFiltrada = listaNum.OrderBy(a => a);
